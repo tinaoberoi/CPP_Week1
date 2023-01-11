904f900055d107f2b948036faef9b11c125a90a9
Tina <tina@Tinas-MacBook-Air.local>
2023-01-11 17:15:49 -0600

    Add solution for vector_demo extra credit

--- hw1.1/vector_simple_demo.cpp
+++ hw1.1/vector_simple_demo.cpp
@@ -6,6 +6,9 @@ void printVec(vector<auto> v) {
     for (int j = 0; j <(int)v.size(); j++)
         cout << v[j] << ", ";
 }
+
+auto square(auto x) { return x * x; }
+
 int main()
 {
     vector v = { 1, 2 }; // Init a vector of ints
